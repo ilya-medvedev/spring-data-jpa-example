c3284762bffb36fce985ab43274e81a25306aa6f
Ilya Medvedev <medved55rus@gmail.com>
2016-08-05 16:47:11 +0600

    remove JsonProperty annotation in UserIdResponse class

--- src/main/java/medvedev/ilya/example/spring/data/jpa/registrator/user/model/json/response/UserIdResponse.java
+++ src/main/java/medvedev/ilya/example/spring/data/jpa/registrator/user/model/json/response/UserIdResponse.java
@@ -1,9 +1,6 @@
 package medvedev.ilya.example.spring.data.jpa.registrator.user.model.json.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class UserIdResponse {
-    @JsonProperty("uid")
     private final String id;
 
     public UserIdResponse(final String id) {
