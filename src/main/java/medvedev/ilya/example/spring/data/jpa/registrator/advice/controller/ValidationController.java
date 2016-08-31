d8100b54e03b4958cce855d31db70f34583dc4d5
Ilya Medvedev <medved55rus@gmail.com>
2016-08-31 18:55:43 +0600

    Remove duplicated code

--- src/main/java/medvedev/ilya/example/spring/data/jpa/registrator/advice/controller/ValidationController.java
+++ src/main/java/medvedev/ilya/example/spring/data/jpa/registrator/advice/controller/ValidationController.java
@@ -12,18 +12,31 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ValidationController {
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ErrorsResponse httpMessageNotReadableException() {
-        return ErrorsResponse.byError("Required request body is wrong");
+    private ErrorsResponse errorsResponseByError(final String error) {
+        final List<String> errors = Collections.singletonList(error);
+
+        return new ErrorsResponse(errors);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    private ErrorsResponse errorsResponseByException(final Exception exception) {
+        final String error = exception.getMessage();
+
+        return errorsResponseByError(error);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ErrorsResponse httpMessageNotReadableException() {
+        return errorsResponseByError("Required request body is wrong");
+    }
+
+    @ExceptionHandler
     public ErrorsResponse methodArgumentNotValidException(final MethodArgumentNotValidException exception) {
         final List<String> errors = exception.getBindingResult()
                 .getAllErrors()
@@ -34,27 +47,21 @@ public class ValidationController {
         return new ErrorsResponse(errors);
     }
 
-    @ExceptionHandler(NoHandlerFoundException.class)
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorsResponse noHandlerFoundException(final NoHandlerFoundException exception) {
-        final String error = exception.getMessage();
-
-        return ErrorsResponse.byError(error);
+        return errorsResponseByException(exception);
     }
 
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     public ErrorsResponse httpMediaTypeNotSupportedException(final HttpRequestMethodNotSupportedException exception) {
-        final String error = exception.getMessage();
-
-        return ErrorsResponse.byError(error);
+        return errorsResponseByException(exception);
     }
 
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
     public ErrorsResponse httpMediaTypeNotSupportedException(final HttpMediaTypeNotSupportedException exception) {
-        final String error = exception.getMessage();
-
-        return ErrorsResponse.byError(error);
+        return errorsResponseByException(exception);
     }
 }
--- src/main/java/medvedev/ilya/example/spring/data/jpa/registrator/advice/response/ErrorsResponse.java
+++ src/main/java/medvedev/ilya/example/spring/data/jpa/registrator/advice/response/ErrorsResponse.java
@@ -1,6 +1,5 @@
 package medvedev.ilya.example.spring.data.jpa.registrator.advice.response;
 
-import java.util.Collections;
 import java.util.List;
 
 public class ErrorsResponse {
@@ -8,13 +7,6 @@ public class ErrorsResponse {
 
     public ErrorsResponse(final List<String> errors) {
         this.errors = errors;
-    }
-
-
-    public static ErrorsResponse byError(final String error) {
-        final List<String> errors = Collections.singletonList(error);
-
-        return new ErrorsResponse(errors);
     }
 
     public List<String> getErrors() {
